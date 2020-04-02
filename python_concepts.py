a1acd79e6e5808e8ffbb716b32c84c2b05880ed5
Krupesh <krupesh.anadkat@xebia.com>
2020-04-02 21:04:09 +0530

    Project 4 looping coffee chatbot

--- python_concepts.py
+++ python_concepts.py
@@ -1,5 +1,35 @@
-# ZIP - merge two or more lists
+#  TRY EXCEPT ----------------------------------------------------------------------------------------------
+try:
+    # some statement
+except ErrorName:
+    # some statement
 
+def raises_value_error():
+    try:
+        print("First I Try this")
+        raise ValueError
+    except ValueError:
+        print("You raised a ValueError!")
+raises_value_error()
+
+# Slice the pizza!
+def pizza_serve(pizza_mass):
+    try:
+        num_slices = int(input("Serve the pizza as how many slices? "))
+        slice_mass = pizza_mass / num_slices
+    except ValueError:  # Input cannot be interpreted as an int.
+        print("Enter an integer, please.")
+        continue
+    except ZeroDivisionError:  # Input was 0.
+        print("Cannot serve it as zero slices.")
+        continue
+    if num_slices < 0:  # Input was negative.
+        print("Cannot serve a negative number of slices.")
+        continue
+    break
+
+
+# ZIP -----------------------------------------------------------------------------------------------
 names = ['Jenny', 'Alexus', 'Sam', 'Grace']
 dogs_names = ['Elphonse', 'Dr. Doggy DDS', 'Carter', 'Ralph']
 
@@ -10,7 +40,7 @@ list_of_names_and_dogs_names = list(names_and_dogs_names)
 print(list_of_names_and_dogs_names)
 
 
-# RANGE - generates numbers starting at 0 and ending at the number before the input.
+# RANGE ---------------------------------------------------------------------------------------------
 # range containing numbers starting at 0 and up to, but not including, 9
 list1 = range(9)
 
@@ -30,7 +60,7 @@ name_and_age = zip(first_names, all_ages)
 ids = range(4)
 
 
-# SLICING ---------------------------------------------------------------------------------
+# SLICING ----------------------------------------------------------------------------------------------
 suitcase = ['shirt', 'shirt', 'pants', 'pants', 'pajamas', 'books']
 beginning = suitcase[0:4]
 middle = suitcase[2:4]
@@ -46,7 +76,7 @@ end = suitcase[-2:]
 mylist = ['a', 'b', 'c', 'd', 'e']
 mylist[1:3]
 
-# COUNT ---------------------------------------------------------------------------------
+# COUNT ------------------------------------------------------------------------------------------------
 votes = ['Jake', 'Jake', 'Laurie', 'Laurie', 'Laurie', 'Jake', 'Jake', 'Jake', 'Laurie', 'Cassie',
          'Cassie', 'Jake', 'Jake', 'Cassie', 'Laurie', 'Cassie', 'Jake', 'Jake', 'Cassie', 'Laurie']
 
@@ -54,7 +84,7 @@ jake_votes = votes.count('Jake')
 
 print(jake_votes)
 
-# SORT ---------------------------------------------------------------------------------
+# SORT ------------------------------------------------------------------------------------------------
 ### Exercise 1 & 2 ###
 addresses = ['221 B Baker St.', '42 Wallaby Way', '12 Grimmauld Place',
              '742 Evergreen Terrace', '1600 Pennsylvania Ave', '10 Downing St.']
@@ -68,13 +98,13 @@ names.sort()
 cities = ['London', 'Paris', 'Rome', 'Los Angeles', 'New York']
 cities.sort()
 
-#  SORTED ---------------------------------------------------------------------------------
+#  SORTED ------------------------------------------------------------------------------------------------
 games = ['Portal', 'Minecraft', 'Pacman', 'Tetris', 'The Sims', 'Pokemon']
 games_sorted = sorted(games)
 print(games)
 print(games_sorted)
 
-# REVIEW ---------------------------------------------------------------------------------
+# REVIEW --------------------------------------------------------------------------------------------------
 inventory = ['twin bed', 'twin bed', 'headboard', 'queen bed', 'king bed', 'dresser', 'dresser', 'table', 'table',
              'nightstand', 'nightstand', 'king bed', 'king bed', 'twin bed', 'twin bed', 'sheets', 'sheets', 'pillow', 'pillow']
 
@@ -88,3 +118,66 @@ first_3 = inventory[:3]
 
 twin_beds = inventory.count('twin bed')
 inventory.sort()
+
+# TUPLE --------------------------------------------------------------------------------------------------
+my_info = ('Mike', 24, 'Programmer')
+my_name = my_info[0]
+my_occ = my_info[-1]
+
+name, age, occ = my_info
+one_element_tuple = (4,)
+
+# LOOPs --------------------------------------------------------------------------------------------------
+"""
+- Loops that let us move through each item in a list, called for loops
+- Loops that keep going until we tell them to stop, called while loops
+- Loops that create new lists, called list comprehensions
+"""
+
+promise = "I will not chew gum in class"
+#  Prints 5 Times
+for i in range(5):
+  print(promise)
+
+students_period_A = ["Alex", "Briana", "Cheri", "Daniele"]
+students_period_B = ["Dora", "Minerva", "Alexa", "Obie"]
+
+for student in students_period_A:
+  students_period_B.append(student)
+  print(student)
+
+# BREAK
+dog_breeds_available_for_adoption = ['french_bulldog', 'dalmatian', 'shihtzu', 'poodle', 'collie']
+dog_breed_I_want = 'dalmatian'
+for dog_breed in dog_breeds_available_for_adoption:
+  print(dog_breed)
+  if dog_breed == dog_breed_I_want:
+    print("They have the dog I want!")
+    break
+
+# CONTINUE
+ages = [12, 38, 34, 26, 21, 19, 67, 41, 17]
+for age in ages:
+  if age<21:
+    continue
+  print(age)
+
+# List Comprehension:
+heights = [161, 164, 156, 144, 158, 170, 163, 163, 157]
+can_ride_coaster = [height for height in heights if height>161]
+print(can_ride_coaster)
+
+celsius = [0, 10, 15, 32, -5, 27, 3]
+fahrenheit = [cel * 9/5 +32  for cel in celsius]
+print(fahrenheit)
+
+# LOOPs REVIEW
+single_digits = list(range(0,10))
+squares = []
+
+for digit in single_digits:
+  squares.append(digit**2)
+print(squares)
+
+cubes = [digit**3 for digit in single_digits]
+print(cubes)
